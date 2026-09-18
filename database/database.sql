97eb5259ff2e6c2a7b6361889d629ffed68b7bc5
Luis Diego Campos Murillo <luisdcampos31@gmail.com>
2026-09-18 15:00:43 -0600

    feat: crear tabla Usuarios AB#16

--- database/database.sql
+++ database/database.sql
@@ -3,3 +3,24 @@ GO
 
 USE LaboratorioOBLD;
 GO
+
+CREATE TABLE Usuarios
+(
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+
+    Nombre NVARCHAR(100) NOT NULL,
+
+    Username NVARCHAR(50) NOT NULL UNIQUE,
+
+    PasswordHash NVARCHAR(255) NOT NULL,
+
+    Rol NVARCHAR(20) NOT NULL,
+
+    IntentosFallidos INT NOT NULL DEFAULT 0,
+
+    BloqueadoHasta DATETIME2 NULL,
+
+    CONSTRAINT CK_Usuarios_Rol
+        CHECK (Rol IN ('Administrador', 'Usuario'))
+);
+GO
