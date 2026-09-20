231377d4b3fe227b72c134979c3700c9aedd1841
oscar <oscarojasg2006@gmail.com>
2026-09-20 15:33:45 -0600

    feat:Mantener Sesión y Validar Roles.

---
+++ Backend/SistemaReservas.API/SistemaReservas.API/Controllers/ReservasController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaReservas.API.Controllers
+{
+    [ApiController]
+    [Route("api/reservas")]
+    public class ReservasController : ControllerBase
+    {
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public IActionResult CrearReserva()
+        {
+            return Ok(new { mensaje = "Reserva creada" });
+        }
+    }
+}
